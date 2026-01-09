d0efd9250cf858760c078606dc2e4ec108898f9d
Naresh Babu Matta <your-mattanareshbabu1998@gmail.com>
2026-01-09 21:34:41 +0530

    logo changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <nav className="navbar">
-      <img src="src/assets/SLogo.png" alt="" className="navbar-logo" />
+      <img src="src/assets/Logo.png" alt="" className="navbar-logo" />
       <h1 className="navbar-title">WELLCOME TO SKYCARE HOSPITAL</h1>
     </nav>
   );
